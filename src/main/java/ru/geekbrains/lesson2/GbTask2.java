0cafd7bb42cd6d35427be854d88353e6d851dfad
m61532 <km61532@gmail.com>
2021-08-05 22:44:35 +0300

    Homework2 + *Task done.

--- src/main/java/ru/geekbrains/lesson2/GbTask2.java
+++ src/main/java/ru/geekbrains/lesson2/GbTask2.java
@@ -1,4 +1,53 @@
 package ru.geekbrains.lesson2;
 
 public class GbTask2 {
+    public static void main(String[] args) {
+
+        System.out.println(checkSum(-10, 15));
+        determinatePrintSign(-10);
+        System.out.println(determinateReturnSign(-3));
+        printString("Hi", 5);
+        System.out.println(determinateLeapYear(2020));
+    }
+
+    private static boolean checkSum(int a, int b) {
+        int sum = a + b;
+
+        if (sum >= 10 & sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void determinatePrintSign (int num) {
+        if (num >= 0){
+            System.out.println(num + " Число положительное");
+        } else {
+            System.out.println(num + " Число отрицательное");
+        }
+    }
+
+    private static boolean determinateReturnSign (int num) {
+        if (num >= 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void printString(String string, int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.println(string);
+        }
+    }
+
+    private static boolean determinateLeapYear (int year) {
+        if (year % 4 == 0 & (year % 100 != 0 | year % 400 == 0)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
